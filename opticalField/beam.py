6a554b13dc7bd9ca5445d99bca8fd4e92afb6eba
carlos-antunis-physics <carlos.bonfim@fis.ufal.br>
2024-03-17 19:50:05 -0300

    Add hermite-gauss beam [update] opticalField/beam.py 1. [add] beam.HermiteGauss() constructor [update] examples/2.optical-beams.ipynb 1. [add] hermite-gauss constructor example

--- opticalField/beam.py
+++ opticalField/beam.py
@@ -18,3 +18,26 @@ def Gaussian(
         incidentWaveK = incidentWavevector,
         normalize = normalize
     );
+
+# hermite-gauss beam constructor
+from scipy.special import eval_hermite as Hermite
+def HermiteGauss(
+    index: tuple[int, int],
+    w0: float,
+    region: tuple[np.ndarray, np.ndarray],
+    center: tuple[float, float] = (0.0, 0.0),
+    phase: float | np.ndarray = 0.0,
+    incidentWavevector: tuple[float, float, float] = (0.0, 0.0, 0.0),
+    normalize: bool = False
+) -> np.ndarray:
+    l, m = index;
+    _sqrt2 = np.sqrt(2);
+    _G = lambda i,S: Hermite(i,S) * np.exp(- S**2 / 2.0);
+    return createField(
+        F = lambda X, Y: _G(l, _sqrt2 * X / w0) * _G(m, _sqrt2 * Y / w0),
+        region = region,
+        center = center,
+        phase = phase,
+        incidentWaveK = incidentWavevector,
+        normalize = normalize
+    );
