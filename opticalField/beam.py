e6f04362f21ab3ecacf637ec17a3498dd0253687
carlos-antunis-physics <carlos.bonfim@fis.ufal.br>
2024-03-17 20:03:42 -0300

    Add laguerre-gauss beam [update] opticalField/beam.py 1. [add] beam.LaguerreGauss() constructor [update] examples/2.optical-beams.ipynb 1. [add] laguerre-gauss constructor example

--- opticalField/beam.py
+++ opticalField/beam.py
@@ -41,3 +41,26 @@ def HermiteGauss(
         incidentWaveK = incidentWavevector,
         normalize = normalize
     );
+
+# laguerre-gauss beam constructor
+from scipy.special import assoc_laguerre as Laguerre
+def LaguerreGauss(
+    index: tuple[int, int],
+    w0: float,
+    region: tuple[np.ndarray, np.ndarray],
+    center: tuple[float, float] = (0.0, 0.0),
+    phase: float | np.ndarray = 0.0,
+    incidentWavevector: tuple[float, float, float] = (0.0, 0.0, 0.0),
+    normalize: bool = False
+) -> np.ndarray:
+    l, m = index;
+    return createField(
+        F = lambda X, Y: (np.sqrt(X**2 + Y **2) / w0) ** l * Laguerre(
+            2 * (X**2 + Y**2), l, m
+        ) * np.exp(-(X**2 + Y**2)/w0**2),
+        region = region,
+        center = center,
+        phase = phase,
+        incidentWaveK = incidentWavevector,
+        normalize = normalize
+    );
